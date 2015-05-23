1c8e690461f9e2648a9bb9c3bd75fc5d1f3db719
David Calle <davidcalle9430@gmail.com>
2015-05-23 08:05:01 -0500

    Created child Process and LinkedList

--- main.c
+++ main.c
@@ -18,17 +18,73 @@ struct Log
 	int idSesion;
 	char accion[50];
 };
+struct Nodo
+{
+	struct Log log;
+	struct Nodo *siguiente;
+};
+struct Lista
+{
+	struct Nodo *cabeza;
+};
+
+struct Nodo* crearNodo(struct Log log){
+	struct Nodo* nuevo = malloc(sizeof(struct Nodo));
+	nuevo->log=log;
+	return nuevo;
+}
+struct Lista* crearLista(){
+	struct Lista *lista= malloc(sizeof(struct Lista));
+	lista->cabeza=NULL;
+	return lista;
+}
+
+void agregarALista(struct Lista* lista, struct Nodo* nodo){
+	struct Nodo* aux=lista->cabeza;
+	if(aux==NULL){
+		lista->cabeza=nodo;
+	}else{
+		while(aux->siguiente!=NULL){
+			aux= aux->siguiente;
+		}
+		aux->siguiente=nodo;
+	}
+
+}
+void eliminarLista(struct Lista *lista){
+	struct Nodo* aux= lista->cabeza;
+	while(aux!=NULL){
+		struct Nodo* actual=aux;
+		aux=aux->siguiente;
+		free(actual);
+	}
+	free(lista);
+}
 /*
 * ¿cómo ingresa el tipo de usuario de cada máquina?
 * ¿Bloggers?
 * se puede ahacer con archivos con nombre pid?
 * toca implementar Lista?
-* que fuck con las sesiones y máquinas?
+* que pasaW con las sesiones y máquinas?
 */
 
 void iniciar_computador(int tipo, int idMaquina){
 	printf(" soy un PC de tipo %i\n",tipo);
 	pthread_t hilo;
+	struct Lista* lista = crearLista();
+	struct Log log,log2;
+	log.idMaquina=1;
+	log2.idMaquina=2;
+	struct Nodo *nodo1,*nodo2;
+	nodo1= crearNodo(log);
+	nodo2= crearNodo(log2);
+	agregarALista(lista, nodo1);
+	agregarALista(lista, nodo2);
+	struct Nodo* aux= lista->cabeza;
+	while(aux!=NULL){
+		printf("LISTA!!! %i %i\n",getpid(),aux->log.idMaquina );
+		aux=aux->siguiente;
+	}
 
 }
 
